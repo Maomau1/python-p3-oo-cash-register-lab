1e7644c27615070f18aeb013a240b33487510544
maurice diong <mdiong00@gmail.com>
2024-01-18 14:44:40 -0500

    interesting little lab. i wonder what other way there is to substract the last item from the total

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self,discount=0):
+    self.discount =  discount
+    self.total = 0
+    self.items = []
+    
+  def add_item(self,item,price, quantity=1):
+    self.last_price = price
+    self.last_quantity = quantity
+    self.total += price*quantity
+    for count in range (0,quantity):
+      self.items.append(item)
+
+  def apply_discount(self):
+    if self.discount == 0:
+      print("There is no discount to apply.")
+    else:
+      self.total = (100-self.discount)/100 * self.total
+      print(f"After the discount, the total comes to ${self.total:.0f}.")
+  
+  def void_last_transaction(self):
+      pass
+      self.total -= self.last_price * self.last_quantity
+  
+    
+    
+
+
+  
